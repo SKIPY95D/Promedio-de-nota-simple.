924308731f6fe23e4d5ba8cba8177ef69795ff5f
SKIPY95D <75347505+SKIPY95D@users.noreply.github.com>
2021-11-11 09:46:09 -0300

    Update and rename promedio.cpp to codigo.cpp

--- codigo.cpp
+++ codigo.cpp
@@ -10,21 +10,22 @@ while (n1 < 1 or n1 > 10){
 	cout<<"la nota es invalida: ";
 	cout<<"ingresala de vuelta: ";
 	cin>>n1;
-};
+}
 cout<<"digite la segunda nota: ";
 cin>>n2;
 while (n2 < 1 or n2 > 10){
 	cout<<"la nota es invalida: ";
 	cout<<"ingresala de vuelta: ";
 	cin>>n2;
-};
+}
 cout<<"digite la tercera nota: ";
 cin>>n3;
 while (n3 < 1 or n3 > 10){
 	cout<<"la nota es invalida: ";
 	cout<<"ingresala de vuelta: ";
 	cin>>n3;
-};
+}
+system("cls"); // clear
 
 promedio = (n1+n2+n3) / 3;
 cout<<"El promedio es: "<<promedio<<endl; //salto de linea
@@ -33,5 +34,5 @@ if (promedio >= 5){ //si es igual o mayor a 5
 	cout<<"estas aprobado :D !!";
 }else{ //sino...
 cout<<"Estas desaprobado :'( ";
-};
+}
 };
